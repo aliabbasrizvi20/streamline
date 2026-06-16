2cbe9bb498e3c2188edae1081d6f1cebf062944c
aliabbasrizvi20 <rockcodes0@gmail.com>
2026-06-16 13:28:48 +0530

    changes

--- src/app/redux/contentThunk.ts
+++ src/app/redux/contentThunk.ts
@@ -1,23 +1,23 @@
-import { AppDispatch } from "./store";
-import { setNews, setMovies, setSocial } from "./contentSlice";
+// import { AppDispatch } from "./store";
+// import { setNews, setMovies, setSocial } from "./contentSlice";
 
-import { getNews } from "../services/newsApi";
-import { getMovies } from "../services/movieapi";
-import { getSocialPosts } from "../services/socialApi";
+// import { getNews } from "../services/newsApi";
+// import { getMovies } from "../services/movieapi";
+// import { getSocialPosts } from "../services/socialApi";
 
-export const loadDashboardContent =
-  () => async (dispatch: AppDispatch) => {
-    try {
-      const [news, movies, social] = await Promise.all([
-        getNews(),
-        getMovies(),
-        getSocialPosts(),
-      ]);
+// export const loadDashboardContent =
+//   () => async (dispatch: AppDispatch) => {
+//     try {
+//       const [news, movies, social] = await Promise.all([
+//         getNews(),
+//         getMovies(),
+//         getSocialPosts(),
+//       ]);
 
-      dispatch(setNews(news));
-      dispatch(setMovies(movies));
-      dispatch(setSocial(social));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//       dispatch(setNews(news));
+//       dispatch(setMovies(movies));
+//       dispatch(setSocial(social));
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
--- src/app/redux/themeSlice.ts
+++ src/app/redux/themeSlice.ts
@@ -1,20 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const themeSlice = createSlice({
-  name: "theme",
+// const themeSlice = createSlice({
+//   name: "theme",
 
-  initialState: {
-    darkMode: true,
-  },
+//   initialState: {
+//     darkMode: true,
+//   },
 
-  reducers: {
-    toggleTheme: (state) => {
-      state.darkMode = !state.darkMode;
-    },
-  },
-});
+//   reducers: {
+//     toggleTheme: (state) => {
+//       state.darkMode = !state.darkMode;
+//     },
+//   },
+// });
 
-export const { toggleTheme } =
-  themeSlice.actions;
+// export const { toggleTheme } =
+//   themeSlice.actions;
 
-export default themeSlice.reducer;
+// export default themeSlice.reducer;
